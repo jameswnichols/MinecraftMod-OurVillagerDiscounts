1b046bbd8485c331266b0d06c62d166533c9951d
James Nichols <jnichols.tigley@gmail.com>
2025-11-03 10:24:32 +0000

    Fixed build to work on Windows.

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,11 +54,7 @@ loom {
     accessWidenerPath.set(file("src/main/resources/ourvillagerdiscounts.accesswidener"))
 }
 
-exec {
-    commandLine("echo", "::set-output name=version::${project.version}")
-}
+println("::set-output name=version::${project.version}")
 
-exec {
-    val minecraftVersion: String by project
-    commandLine("echo", "::set-output name=minecraftVersion::${minecraftVersion}")
-}
+val minecraftVersion: String by project
+println("::set-output name=minecraftVersion::${minecraftVersion}")
